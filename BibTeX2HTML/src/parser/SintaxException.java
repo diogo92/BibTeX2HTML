245c34d78620f25f64e3a0973b7c157f2465f620
Diogo Eiras <eiras@outlook.com>
2015-04-18 12:59:12 +0100

    Árvore refeita; Parsing não está completo

---
+++ BibTeX2HTML/src/parser/SintaxException.java
@@ -0,0 +1,14 @@
+package parser;
+
+public class SintaxException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2393017077025597451L;
+
+	public SintaxException(String msg) {
+		super(msg);
+	}
+
+}

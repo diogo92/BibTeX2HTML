ba5cbab5a90f31b46b451f312106cb1768ed1977
diogo92 <diogo_p_ribeiro@hotmail.com>
2015-05-05 22:09:23 +0100

    symbol table

---
+++ BibTeX2HTML/src/data/SymbolTable.java
@@ -0,0 +1,14 @@
+package data;
+
+public class SymbolTable extends Object {
+
+
+  public String type;  
+  public String value; 
+
+  public SymbolTable(String itype, String ivalue) {
+    type=itype;
+    value=ivalue;
+
+  }
+}

010d7a67d661c3d501593143e1f144f207802792
Diogo Eiras <eiras@outlook.com>
2015-05-10 17:24:48 +0100

    Some semantic check

---
+++ BibTeX2HTML/src/parser/SemanticException.java
@@ -0,0 +1,14 @@
+package parser;
+
+public class SemanticException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5060230310080779968L;
+
+	public SemanticException(String message) {
+		super(message);
+	}
+
+}
